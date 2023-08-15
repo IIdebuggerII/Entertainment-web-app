4d742a7186991919cc1f071bc1c2b9f6fb7cba9d
debugger <90983133+IIdebuggerII@users.noreply.github.com>
2023-08-15 15:37:33 +0330

    scroll bar

--- src/component/Scroller.jsx
+++ src/component/Scroller.jsx
@@ -1,65 +1,19 @@
 import React from "react";
+import Scrolllitem from "./scrolllitem";
 
 export default function Scroller() {
   return (
     <div className="mt-5">
       <p className="text-2xl font-light">Trending</p>
-      <div className="container  min-w-full overflow-x-auto">
-        <div className=" flex flex-row ">
-          <div className="w-82">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd8dS_WZaNHs7smjneukcVsg1u_crJSuXjouns64tkXUQbyDNyVkTFU-d1BH54UlPF9KY&usqp=CAU"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd8dS_WZaNHs7smjneukcVsg1u_crJSuXjouns64tkXUQbyDNyVkTFU-d1BH54UlPF9KY&usqp=CAU"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd8dS_WZaNHs7smjneukcVsg1u_crJSuXjouns64tkXUQbyDNyVkTFU-d1BH54UlPF9KY&usqp=CAU"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd8dS_WZaNHs7smjneukcVsg1u_crJSuXjouns64tkXUQbyDNyVkTFU-d1BH54UlPF9KY&usqp=CAU"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd8dS_WZaNHs7smjneukcVsg1u_crJSuXjouns64tkXUQbyDNyVkTFU-d1BH54UlPF9KY&usqp=CAU"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd8dS_WZaNHs7smjneukcVsg1u_crJSuXjouns64tkXUQbyDNyVkTFU-d1BH54UlPF9KY&usqp=CAU"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd8dS_WZaNHs7smjneukcVsg1u_crJSuXjouns64tkXUQbyDNyVkTFU-d1BH54UlPF9KY&usqp=CAU"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd8dS_WZaNHs7smjneukcVsg1u_crJSuXjouns64tkXUQbyDNyVkTFU-d1BH54UlPF9KY&usqp=CAU"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd8dS_WZaNHs7smjneukcVsg1u_crJSuXjouns64tkXUQbyDNyVkTFU-d1BH54UlPF9KY&usqp=CAU"
-              alt=""
-            />
-          </div>
+      <div className=" container overflow-auto">
+        <div className="flex flex-row  ">
+          <Scrolllitem/>
+          <Scrolllitem/>
+          <Scrolllitem/>
+          <Scrolllitem/>
+          <Scrolllitem/>
+          <Scrolllitem/>
+          <Scrolllitem/>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
+    extend: {colors: {
       darkblue: "rgb(22,29,47)",
       dark: "rgb(16,20,31)",
-    },
-    extend: {},
+    },}
   },
   plugins: [],
 };
