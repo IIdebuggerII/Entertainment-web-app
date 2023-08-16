4299875b9847507149321d442a446b28729cb2a8
debugger <90983133+IIdebuggerII@users.noreply.github.com>
2023-08-16 13:24:03 +0330

    fix scroller

--- src/component/Scrolllitem.jsx
+++ src/component/Scrolllitem.jsx
@@ -5,14 +5,8 @@ export default function Scrolllitem() {
 
     <div className='min-w-[300px]  mr-4 relative ' >
         
-        <img className='w-full rounded-lg my-2' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6ntKRpMo5rNFiNBlvTTdneUlcJmqLd-R79Q&usqp=CAU" alt="" />
-        <a href="">
-        <span className='absolute top-4 right-1 z-10 bg-darkblue bg-opacity-60 p-1 rounded-full'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
-            </svg>
-            
-        </span>
-        </a>
+        
+        
         <div className='w-1/3 absolute bottom-2 left-4 flex flex-col'>
             <div className='flex flex-row items-center '>
                 <p className='opacity-60 '>2019 </p> 
